c23f28b48a5886d29a85561b8c257ff9a52fc165
rabloom <rachel.g.bloom@gmail.com>
2015-02-25 16:59:39 -0500

    new style kml

--- Assignment2/formatscrape.py
+++ Assignment2/formatscrape.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('calgarycsv.csv','r') as f:
+with open('calgarycsv.csv','rU') as f:
     geog_csv = csv.reader(f)
     for row in geog_csv:
         if row[2]=="Price":
@@ -15,4 +15,3 @@ with open('calgarycsv.csv','r') as f:
             print ("<Placemark>\n    <name/>\n    <styleUrl>#style3</styleUrl>\n    <description/>\n    <ExtendedData>\n        <Data name='price'>\n            <value>%s</value>\n        </Data>\n        <Data name='bed'>\n            <value>%s</value>\n        </Data>\n        <Data name='bath'>\n            <value>%s</value>\n        </Data>\n        <Data name='sqfoot'>\n            <value>%s</value>\n        </Data>\n        <Data name='image'>\n            <value>%s</value>\n        </Data>\n        <Data name='outputaddress'>\n            <value>%s</value>\n        </Data>\n        <Data name='URL'>\n            <value>%s</value>\n        </Data>\n    </ExtendedData>\n    <Point>\n        <coordinates>\n            %s,%s\n        </coordinates>\n    </Point>\n</Placemark>\n"%(row[2],row[5],row[6],row[7],row[8],row[11],row[1],row[10],row[9]))
         else:
             print ("<Placemark>\n    <name/>\n    <styleUrl>#style4</styleUrl>\n    <description/>\n    <ExtendedData>\n        <Data name='price'>\n            <value>%s</value>\n        </Data>\n        <Data name='bed'>\n            <value>%s</value>\n        </Data>\n        <Data name='bath'>\n            <value>%s</value>\n        </Data>\n        <Data name='sqfoot'>\n            <value>%s</value>\n        </Data>\n        <Data name='image'>\n            <value>%s</value>\n        </Data>\n        <Data name='outputaddress'>\n            <value>%s</value>\n        </Data>\n        <Data name='URL'>\n            <value>%s</value>\n        </Data>\n    </ExtendedData>\n    <Point>\n        <coordinates>\n            %s,%s\n        </coordinates>\n    </Point>\n</Placemark>\n"%(row[2],row[5],row[6],row[7],row[8],row[11],row[1],row[10],row[9]))
-
